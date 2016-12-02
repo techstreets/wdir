e0b958ce764c20f60f7f85e5756bf58706310182
Bojan Radojevic <bradojevic@gmail.com>
2016-12-02 15:07:48 +0100

    Init commit

---
+++ runner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import sys
+
+
+def main():
+    print sys.argv
+
+
+if __name__ == '__main__':
+    main()
